7ee2e2c557d56c8f034f2b6a5f55bce77ba0e527
Daniele Cocca <daniele.cocca@gmail.com>
2020-08-06 13:14:06 +0100

    run.sh: exit on errors

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+set -eu
 
 readonly _GRESOURCE='data/com.github.jmc-88.bifrost.gresource'
 
+python3 setup.py build || exit 1
+
 env _DEBUG_RESOURCE_PATH="$(realpath "${_GRESOURCE}")" \
-  python3 -m src.bifrost
+  python3 -m src.bifrost || exit 1
